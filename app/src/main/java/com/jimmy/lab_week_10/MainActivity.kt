059e24825dfff652fcb88e8f7f8ea3898f659bdf
Jimagination <jimmytandra65@gmail.com>
2025-11-17 23:48:56 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/src/main/java/com/jimmy/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/jimmy/lab_week_10/MainActivity.kt
@@ -5,16 +5,27 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.jimmy.lab_week_10.database.Total
+import com.jimmy.lab_week_10.database.TotalDatabase
 import com.jimmy.lab_week_10.viewmodels.TotalViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val db by lazy { prepareDatabase() }
+
     private val viewModel by lazy {
         ViewModelProvider(this)[TotalViewModel::class.java]
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeValueFromDatabase()
         prepareViewModel()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update(Total(ID, viewModel.total.value!!))
     }
     private fun updateText(total: Int) {
         findViewById<TextView>(R.id.text_total).text =
@@ -27,6 +38,28 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
+    }
+
+    private fun prepareDatabase() : TotalDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java, "total-database"
+        ).allowMainThreadQueries().build()
+    }
+
+    private fun initializeValueFromDatabase(){
+        Thread{
+            val total = db.totalDao().getTotal(ID)
+            if(total.isEmpty()){
+                db.totalDao().insert(Total(id = 1, total = 0))
+            } else {
+                viewModel.setTotal(total.first().total)
+            }
+        }.start()
+    }
+
+    companion object{
+        const val ID : Long = 1
     }
 }
 
